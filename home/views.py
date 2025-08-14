154ced64762591abbae1d8daeaecfb1f4c0319bd
shanith07 <shanithshaji171@gmail.com>
2025-08-14 15:30:39 +0530

    30562

--- home/views.py
+++ home/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest-framework .respone import Response
+from rest-framework .views import APIView
+from .serializers import RestaurantInfoSerializer
+
 
 # Create your views here.
+class HomepageView(APIView):
+    def get(self, request):
+        info=RestaurantInfo.objects.first()
+        if info:
+            serializer=RestaurantInfoSerializer(info)
+            return Response(serializer.data)
+        return Response({"name": "Restaurant name not set"})
